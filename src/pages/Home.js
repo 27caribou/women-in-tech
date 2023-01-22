26cb6f5c821e0ad1458285c9bfb3af45ef802c9d
CandiceWilliams <canwill2511@gmail.com>
2023-01-22 02:53:19 -0500

    changed font and spacing. added buttons for workshops

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -8,7 +8,7 @@ export const Home = () => {
     return (
         <>
             <div className = "div1">
-                <h1 className='head'>Women In Tech Summit 2023</h1>
+                <h1 className='head'>Women In Tech <span className='line-break'>Summit 2023</span></h1>
             </div>
             <div>
                 <div className='div2'>
@@ -16,7 +16,7 @@ export const Home = () => {
                     <p className='text'>Provided paragraph</p>
                 </div>
 
-                <div className='div2'>
+                <div className='div3'>
                     <h2 className='text'>Workshops & Events</h2>
                     <button className='workshop-button'>Workshop 1</button>
                     <div className='divider'/>
@@ -24,6 +24,13 @@ export const Home = () => {
                     <div className='divider'/>
                     <button className='workshop-button'>Summit</button>
                 </div>
+            </div>
+            <h2 className='event-heading'>What will event day look like?</h2>
+            <div className='event-container'>
+                <button className='booths'>Networking Booths</button>
+                <button className='competition'>Interview Competition</button>
+                <button className='speakers'>Keynote Speakers</button>
+                <button className='etc'>etc</button>
             </div>
         </>
     )
